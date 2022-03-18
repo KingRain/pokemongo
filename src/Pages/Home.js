eb6519a9aee4b9c5a9ddecd2577676f8cd9a7af0
RainBoi <samjoe55555@gmail.com>
2022-03-18 07:29:47 +0000

    fixess

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,12 +4,10 @@
 import HeroSection from "../Sections/Hero/index";
 import About from "../Sections/About/index";
 //import Services from "../Sections/Services/index";
-//import Testimonials from "../Sections/Testimonials/index";
 import Contact from "../Sections/Contact/index";
 import styled from "styled-components";
 import About2 from "../Sections/About2/index";
 import About3 from "../Sections/About3/index";
-import Testimonials from "../Sections/Testimonials/index";
 
 import React from 'react';
 
@@ -30,7 +28,6 @@ const Home = () => {
       <About />
       <About2 />
       <About3 />
-      <Testimonials />
       <Contact />
     </Container>
   );
